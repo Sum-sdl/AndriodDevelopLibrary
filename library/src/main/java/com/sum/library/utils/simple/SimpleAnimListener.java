119cf31f9b95616f1ba75548224bfa5932c17a99
Sum-sdl <1079569023@qq.com>
2018-09-11 15:35:49 +0800

    update

---
+++ library/src/main/java/com/sum/library/utils/simple/SimpleAnimListener.java
@@ -0,0 +1,24 @@
+package com.sum.library.utils.simple;
+
+import android.animation.Animator;
+
+/**
+ * Created by sdl on 2018/9/11.
+ */
+public abstract class SimpleAnimListener implements Animator.AnimatorListener {
+
+    @Override
+    public void onAnimationStart(Animator animation) {
+
+    }
+
+    @Override
+    public void onAnimationCancel(Animator animation) {
+
+    }
+
+    @Override
+    public void onAnimationRepeat(Animator animation) {
+
+    }
+}
---
+++ library/src/main/java/com/sum/library/utils/simple/SimpleViewpagerChangeListener.java
@@ -0,0 +1,18 @@
+package com.sum.library.utils.simple;
+
+import android.support.v4.view.ViewPager;
+
+/**
+ * Created by sdl on 2018/9/11.
+ */
+public abstract class SimpleViewpagerChangeListener implements ViewPager.OnPageChangeListener {
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
+    }
+}
